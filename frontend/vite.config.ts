11dd0c8016b6e16b7ad48e3cb8ffad00a0f337d0
AndreBauzil <Andregbauzil@gmail.com>
2025-10-01 20:05:00 -0300

    chore: initialize project with python backend and react frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+// frontend/vite.config.ts
+import path from "path" 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Adicione o objeto 'resolve' abaixo
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
